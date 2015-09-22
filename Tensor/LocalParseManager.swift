5d32f5c7a300c90b54d2b7e2c9565717c113c4b0
nmelehan <nmelehan@gmail.com>
2015-09-22 17:19:45 -0400

    Created class for managing local parse datastore

---
+++ Tensor/LocalParseManager.swift
@@ -0,0 +1,16 @@
+//
+//  LocalParseManager.swift
+//  Tensor
+//
+//  Created by Nathan Melehan on 9/22/15.
+//  Copyright © 2015 Nathan Melehan. All rights reserved.
+//
+
+import Foundation
+
+class LocalParseManager
+{
+    static let sharedManager = LocalParseManager()
+    
+    
+}
